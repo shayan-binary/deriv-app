ab7b37f9378cbe6be6b587eb89d410e6711e3673
shayan <shayanypn@shayans-MacBook-Pro.local>
2019-06-14 06:54:35 +0430

    Deploy to prop-types

---
+++ br_prop-types/js/60-70d541ff15fb8d23d5ea.js
@@ -0,0 +1,49 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[60],{
+
+/***/ "./src/javascript/app/Assets/Trading/Barriers/icon-barrier-up.jsx":
+/*!************************************************************************!*\
+  !*** ./src/javascript/app/Assets/Trading/Barriers/icon-barrier-up.jsx ***!
+  \************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _propTypes = __webpack_require__(/*! prop-types */ "./node_modules/prop-types/index.js");
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+var _react = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var IconBarrierUp = function IconBarrierUp(_ref) {
+    var className = _ref.className;
+    return _react2.default.createElement(
+        'svg',
+        { className: className, width: '16', height: '16' },
+        _react2.default.createElement('path', {
+            className: 'color1-fill',
+            fillOpacity: '.8',
+            fillRule: 'evenodd',
+            d: 'M8.5 4.207V12.5a.5.5 0 1 1-1 0V4.207L4.854 6.854a.5.5 0 1 1-.708-.708l3.5-3.5a.5.5 0 0 1 .708 0l3.5 3.5a.5.5 0 1 1-.708.708L8.5 4.207z'
+        })
+    );
+};
+
+IconBarrierUp.propTypes = {
+    className: _propTypes2.default.string
+};
+
+exports.default = IconBarrierUp;
+
+/***/ })
+
+}]);
